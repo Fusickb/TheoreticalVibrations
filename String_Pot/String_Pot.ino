92923d8a7c3ccc2d998ee85f1cd0a79442a77599
Nathanael Rake <nathanael-rake@utulsa.edu>
2016-09-22 16:57:43 -0500

    Made the string potentiometer code work
    I have not been able to test the CAN portion, but I have verified that it does function over serial.

--- String_Pot/String_Pot.ino
+++ String_Pot/String_Pot.ino
@@ -4,57 +4,95 @@
 // Ram Truck Shock Testing
 
 
-
+//**********************************************************************
+//Includes
 #include <FlexCAN.h>
 
-#define analogPin   20
-#define ledPin      13
 
-FlexCAN CANbus(250000);
-static CAN_message_t txmsg;
+//**********************************************************************
+//Defines
+  //Pines
+  #define analogPin   20
+  #define ledPin      13
 
-elapsedMillis canTimer;
+  //Serial
+  #define SERIAL_BAUD         9600
 
-int16_t stringPot = 0;
-int8_t stringPotLSB = 0;
-int8_t stringPotMSB = 0;
+  //CAN
+  #define id0                 0x788
+  #define CAN_BAUD            250000
+
+
+//**********************************************************************
+//Initializations
+  //CAN
+  FlexCAN CANbus(CAN_BAUD);
+  static CAN_message_t txmsg;
+
+  //Timers
+  elapsedMillis canTimer;
+  elapsedMillis ledTimer;
+
+  //String Pot Analog Variables
+  uint16_t stringPot = 0;
+  uint8_t stringPotLSB = 0;
+  uint8_t stringPotMSB = 0;
 
 boolean ledState = false;
 
+
+//**********************************************************************
+//Setup
 void setup() {
-  // put your setup code here, to run once:
+  //LED pin
+  pinMode(ledPin, OUTPUT);
   
-  CANbus.begin ();
+  //Setup Serial
+  Serial.begin(SERIAL_BAUD);
+
+  //Setup CAN Bus
+  CANbus.begin();
 
 }
 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//**********************************************************************
+//Main
+void loop(){
+
+  if(ledTimer >= 500){
+    ledTimer = 0;
+    ledState = !ledState;
+    digitalWrite(ledPin,ledState);
+  }
   
   if (canTimer >= 10){ 
+    canTimer = 0; //reset the timer
+    
     stringPot = analogRead(analogPin);
     stringPotLSB = stringPot; //stores last 8 bits when casting 16 bit int to 8 bit int
     stringPotMSB = stringPot >> 8; //shifts 8 bits and stores the first 8 bits
-    
-    canTimer = 0;
-    txmsg.id = 0x788;                                                    // Create a sendable message with the CAN id 0x788, Front String Pot
+
+    //DEBUG: serial printing
+    //Serial.print(stringPot);
+    //  Serial.print("\t");
+    //Serial.print(stringPotLSB,HEX);
+    //  Serial.print("\t");
+    //Serial.println(stringPotMSB,HEX);
+
+       
+    txmsg.id = id0;                                                    // Create a sendable message with the CAN id 0x788, Front String Pot
     txmsg.len = 2;                                                       // that has a length of 2 bytes,
     txmsg.ext = 1;                                                       // that also has an extension (?) of 1.
 
     txmsg.buf[0] = stringPotLSB;
     txmsg.buf[1] = stringPotMSB;
-
-    ledState = !ledState;
-    digitalWrite(ledPin,ledState);
-    
-   
     CANbus.write(txmsg);                                                    // Send this message out on the CAN bus.
-
+    
   }
-
-
-
-  
-
 }
+
+
+
+
+
